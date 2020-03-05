457777b85282908d35e0a41138fa34359e097449
edris ranjbar <edris.qeshm2@gmail.com>
2020-03-05 16:55:52 +0330

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Self Analyzer
+this is a personal project to improve my skills change myself to a new bettwer version.
---
+++ main.py
@@ -0,0 +1,18 @@
+# tasks that I should do every single day
+tasks = [
+    'Sleep early in the night',
+    'Wake up early in the morning',
+    'Read Quran and think about it',
+    'Drink 1 liter water per day',
+    'Eat vegetables',
+    'Make a plan for next day',
+    'Improve relationships',
+    'Stay happy and laugh',
+    'Learn English',
+    'Exercise',
+    'Study everyday',
+    'Code everyday',
+    'Help others'
+]
+for task in tasks:
+    print("Did you done " + task)
