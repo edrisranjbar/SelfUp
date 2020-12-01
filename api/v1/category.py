bb0384fef73862621bdc107c30db8fdefe984f26
Edris Ranjbar <edris.qeshm2@gmail.com>
2020-12-01 20:11:30 +0330

    Working on back-end and Restfull API service

--- api/v1/category.py
+++ api/v1/category.py
@@ -7,14 +7,11 @@ import config
 class Category:
     """ To manage task categories """
 
-    def __init__(self):
-        self.create_table()
-
     @staticmethod
     def create_table():
         connection = connect(config.db_file)
         connection.execute("""CREATE TABLE IF NOT EXISTS category
-                (id integer PRIMARY KEY, name TEXT, description TEXT)""")
+                (id integer PRIMARY KEY, name TEXT, description TEXT, user_id integer, FOREIGN KEY(user_id) REFERENCES user(user_id))""")
         connection.close()
         return True
 
@@ -29,25 +26,24 @@ class Category:
         else:
             return False
 
-    
     @staticmethod
-    def get_count():
+    def get_count(user_id):
         conn = connect(config.db_file)
-        query = "SELECT count(*) FROM category"
+        query = f"SELECT count(*) FROM category WHERE user_id = {user_id}"
         count = conn.execute(query).fetchone()[0]
         conn.close()
         return count
 
     @staticmethod
-    def add(name, description):
+    def add(name, description, user_id):
         """ Add a new category with some details """
         connection = connect(config.db_file)
         cursor = connection.cursor()
-        cursor.execute(f'INSERT INTO category (name,description) VALUES ("{name}","{description}")')
+        cursor.execute(
+            f'INSERT INTO category (name,description, user_id) VALUES ("{name}","{description}", {user_id})')
         connection.commit()
         connection.close()
         return True
-
 
     @staticmethod
     def delete(category_id):
@@ -65,23 +61,22 @@ class Category:
         return True
 
     @staticmethod
-    def update(category_id, category_name, description=""):
+    def update(category_id, user_id, category_name, description=""):
         connection = connect(config.db_file)
         cursor = connection.cursor()
         if description is None:
-            query = f"UPDATE category SET name='{category_name}' WHERE id={category_id}"
+            query = f"UPDATE category SET name='{category_name}' WHERE id={category_id} AND user_id = {user_id}"
         else:
-            query = f"UPDATE category SET name='{category_name}', description='{description}' WHERE id={category_id}"
+            query = f"UPDATE category SET name='{category_name}', description='{description}' WHERE id={category_id} AND user_id = {user_id}"
         cursor.execute(query)
         connection.commit()
         return True
 
-
     @staticmethod
-    def get_all():
+    def get_all(user_id):
         """ Get all of categories and returns an Array """
         connection = connect(config.db_file)
-        query = "SELECT * FROM category"
+        query = f"SELECT * FROM category WHERE user_id = {user_id}"
         try:
             categories: List[Any] = connection.execute(query).fetchall()
         except Error:
@@ -90,6 +85,7 @@ class Category:
             connection.close()
         categories_array = []
         for category in categories:
-            category_dict = {'id': category[0], 'name': category[1], 'description': category[2]}
+            category_dict = {
+                'id': category[0], 'name': category[1], 'description': category[2], 'user_id': category[3]}
             categories_array.append(category_dict)
         return categories_array
